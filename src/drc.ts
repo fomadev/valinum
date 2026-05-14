0593b58e630ecef4607a3e66076d5bd735bb616a
Fordi Malanda <fordimalanda7@gmail.com>
2026-05-14 10:42:05 +0200

    chore: migrate license from MIT to FomaDev Public License (FPL)

--- src/drc.ts
+++ src/drc.ts
@@ -1,3 +1,8 @@
+/* * Copyright (c) 2026 Fordi / FomaDev. 
+ * Licensed under FomaDev Public License.
+ * See LICENSE file in the project root for full license information.
+ */
+
 import { ValidationResult, ValidationOptions } from './types';
 
 const OPERATORS_RDC: { [key: string]: string } = {
--- src/types.ts
+++ src/types.ts
@@ -1,3 +1,8 @@
+/* * Copyright (c) 2026 Fordi / FomaDev. 
+ * Licensed under FomaDev Public License.
+ * See LICENSE file in the project root for full license information.
+ */
+
 export interface ValidationResult {
   isValid: boolean;     // True si 9 chiffres exacts ET opérateur connu
   operator: string | null; // Vodacom, Orange, Airtel, Africell
